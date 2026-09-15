779b72abb77a055775faf74e5a72915c54ca8197
Mustafa Ali Khan <mustafaalikhan2570@gmail.com>
2026-09-15 22:52:29 +0300

    hygiene: drop password scratch scripts and local compose credentials from public history going forward

--- erpnext_scripts/m4_pw.py
+++
@@ -1,7 +0,0 @@
-import frappe
-from frappe.utils.password import update_password
-for u in ["purchase.manager@alrehab-demo.example", "accounts.manager@alrehab-demo.example"]:
-    update_password(user=u, pwd="demo123")
-    print("pw set", u)
-frappe.db.commit()
-print("PW DONE")
